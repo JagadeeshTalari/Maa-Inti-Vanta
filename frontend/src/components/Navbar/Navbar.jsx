768f7fb8fccec35ce4fa336502f6e5e2c23e1a7e
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2025-02-09 22:51:53 +0530

    updated navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,48 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar">
+      <h5 className="footer-logo">Maa inti vanta</h5>
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mob-app")}
+          className={menu === "mob-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact")}
+          className={menu === "contact" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div to="/cart" className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className={"dot"}></div>
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
